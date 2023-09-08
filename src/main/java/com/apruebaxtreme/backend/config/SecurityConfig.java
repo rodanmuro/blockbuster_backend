6c02581a1a1c376ddcb7969dd308ab71b2b56de8
rodanmuro <rodanmuro@gmail.com>
2023-09-08 17:14:12 -0500

    26. Securizando los Endpint con JWT. Agregando dependencia y creando la utilidad JWT

--- src/main/java/com/apruebaxtreme/backend/config/SecurityConfig.java
+++ src/main/java/com/apruebaxtreme/backend/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -13,6 +14,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterSecurity(HttpSecurity http) throws Exception{
 
+        
         http
         .csrf(
             csrf->csrf.disable()
